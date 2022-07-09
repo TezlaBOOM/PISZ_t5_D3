69d3955ff0792aca2bff4ca28dff85dcb45620de
Witold Michalak <Witold3152000@wp.pl>
2022-07-09 18:59:09 +0200

    1

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ void info(){
     cout<<"Reserved1: "<<File.Reserved1<<endl;
     cout<<"Reserved2: "<<File.Reserved2<<endl;
     cout<<"Pozycja danych obrazkowych: "<<File.OffBits<<endl;
-    
+
     cout<<endl;
 
     fseek(f, 14, SEEK_SET);
@@ -83,10 +83,76 @@ void info(){
     fclose(f);
     }
 }
+void negatyw(){
+    FILE* f = fopen("test.bmp", "rb");
+
+    if (f == nullptr)
+    {
+        cout<<"Nie można otworzyć tego pliku"<<endl;     
+    }
+    else
+    {
+       cout<<"Plik jest otwary poprawnie"<<endl;
+    
+     FILE* w = fopen("negatyw_test.bmp", "wb");
+    if (w == nullptr)
+    {
+        cout<<"Nie można otworzyć tego pliku"<<endl;
+    }
+    else
+    {
+        cout<<"Plik jest otwary poprawnie"<<endl;
+        cout<<"negatyw jest wtorzony..."<<endl;
+        
+
+        fseek(w, 0, SEEK_SET);
+        fwrite(&File.Type, sizeof(File.Type), 1, w);
+        fwrite(&File.Size, sizeof(File.Size), 1, w);
+        fwrite(&File.Reserved1, sizeof(File.Reserved1), 1, w);
+        fwrite(&File.Reserved2, sizeof(File.Reserved2), 1, w);
+        fwrite(&File.OffBits, sizeof(File.OffBits), 1, w);
+
+        fseek(w, 14, SEEK_SET);
+        fwrite(&Picture.Size, sizeof(Picture.Size), 1, w);
+        fwrite(&Picture.Width, sizeof(Picture.Width), 1, w);
+        fwrite(&Picture.Height, sizeof(Picture.Height), 1, w);
+        fwrite(&Picture.Planes, sizeof(Picture.Planes), 1, w);
+        fwrite(&Picture.BitCount, sizeof(Picture.BitCount), 1, w);
+        fwrite(&Picture.Compression, sizeof(Picture.Compression), 1, w);
+        fwrite(&Picture.SizeImage, sizeof(Picture.SizeImage), 1, w);
+        fwrite(&Picture.XPelsPerMeter, sizeof(Picture.XPelsPerMeter), 1, w);
+        fwrite(&Picture.YPelsPerMeter, sizeof(Picture.YPelsPerMeter), 1, w);
+        fwrite(&Picture.ClrUsed, sizeof(Picture.ClrUsed), 1, w);
+        fwrite(&Picture.ClrImportant, sizeof(Picture.ClrImportant), 1, w);
+
+        fseek(w, sizeof(File.OffBits), SEEK_SET);
+
+        int bmpImg;
+        for (int i = File.OffBits; i < File.Size; i++)
+        {
+            fseek(f, i, SEEK_SET);
+            fseek(w, i, SEEK_SET);
+            fread(&bmpImg, 3, 1, f);
+            bmpImg = INT_MAX - bmpImg; 
+            fwrite(&bmpImg, 3, 1, w);
+
+            
+        }
+
+        cout<<"proces przebiegł pomyśnie"<<endl;
+       
+    }
+    fclose(f);
+    fclose(w);
+
+    }
+}
+
+
 int main(int arc, char * argv[]) {
 
 
     info();
-    
+    negatyw();
     return 0;
 }
