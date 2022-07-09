94e28c638361923e6b57f083fa23ad53d33a13c5
Witold Michalak <Witold3152000@wp.pl>
2022-07-09 18:55:24 +0200

    0.5

--- main.cpp
+++ main.cpp
@@ -3,13 +3,28 @@
 #include <limits>
 
 using namespace std;
-struct Plik_Naglowek {
+struct File_Header {
     short Type;
     int Size;
     short Reserved1;
     short Reserved2;
     int OffBits;
-} Plik;
+} File;
+
+struct Picture_Header {
+    int Size;
+    int Width;
+    int Height;
+    short Planes;
+    short BitCount;
+    int Compression;
+    int SizeImage;
+    int XPelsPerMeter;
+    int YPelsPerMeter;
+    int ClrUsed;
+    int ClrImportant;
+} Picture;
+
 
 void info(){
     FILE* f = fopen("test.bmp", "rb");
@@ -28,16 +43,42 @@ void info(){
     cout<<" ===================== "<<endl;
 
 
-    fread(&Plik.Type, sizeof(Plik.Type), 1, f);
-    fread(&Plik.Size, sizeof(Plik.Size), 1, f);
-    fread(&Plik.Reserved1, sizeof(Plik.Reserved1), 1, f);
-    fread(&Plik.Reserved2, sizeof(Plik.Reserved2), 1, f);
-    fread(&Plik.OffBits, sizeof(Plik.OffBits), 1, f);
-    cout<<"Typ: "<<Plik.Type<<endl;
-    cout<<"rozmiar pliku: " << Plik.Size << " bajtow" << endl;
-    cout<<"Reserved1: "<<Plik.Reserved1<<endl;
-    cout<<"Reserved2: "<<Plik.Reserved2<<endl;
-    cout<<"Pozycja danych obrazkowych: "<<Plik.OffBits<<endl;
+    fread(&File.Type, sizeof(File.Type), 1, f);
+    fread(&File.Size, sizeof(File.Size), 1, f);
+    fread(&File.Reserved1, sizeof(File.Reserved1), 1, f);
+    fread(&File.Reserved2, sizeof(File.Reserved2), 1, f);
+    fread(&File.OffBits, sizeof(File.OffBits), 1, f);
+    cout<<"Typ: "<<File.Type<<endl;
+    cout<<"rozmiar pliku: " << File.Size << " bajtow" << endl;
+    cout<<"Reserved1: "<<File.Reserved1<<endl;
+    cout<<"Reserved2: "<<File.Reserved2<<endl;
+    cout<<"Pozycja danych obrazkowych: "<<File.OffBits<<endl;
+    
+    cout<<endl;
+
+    fseek(f, 14, SEEK_SET);
+    fread(&Picture.Size, sizeof(Picture.Size), 1, f);
+    fread(&Picture.Width, sizeof(Picture.Width), 1, f);
+    fread(&Picture.Height, sizeof(Picture.Height), 1, f);
+    fread(&Picture.Planes, sizeof(Picture.Planes), 1, f);
+    fread(&Picture.BitCount, sizeof(Picture.BitCount), 1, f);
+    fread(&Picture.Compression, sizeof(Picture.Compression), 1, f);
+    fread(&Picture.SizeImage, sizeof(Picture.SizeImage), 1, f);
+    fread(&Picture.XPelsPerMeter, sizeof(Picture.XPelsPerMeter), 1, f);
+    fread(&Picture.YPelsPerMeter, sizeof(Picture.YPelsPerMeter), 1, f);
+    fread(&Picture.ClrUsed, sizeof(Picture.ClrUsed), 1, f);
+    fread(&Picture.ClrImportant, sizeof(Picture.ClrImportant), 1, f);
+    cout<<"Wielkosc naglowka informacyjnego: "<<Picture.Size<<endl;
+    cout << "Szerokosc: " << Picture.Width << " pikseli" << endl;
+    cout << "Wysokosc: " << Picture.Height << " pikseli" << endl;
+    cout<<"Liczba platow: "<<Picture.Planes<<endl;
+    cout<<"Liczba bitow na piksel: "<<Picture.BitCount<<endl;
+    cout<<"Kompresja: "<<Picture.Compression<<endl;
+    cout<<"Rozmiar obrazka: "<<Picture.SizeImage<<endl;
+    cout<<"Liczba pikseli na metr: "<<Picture.XPelsPerMeter<<endl;
+    cout<<"Liczba pikseli na metr: "<<Picture.YPelsPerMeter<<endl;
+    cout<<"Liczba uzytych kolorow: "<<Picture.ClrUsed<<endl;
+    cout<<"Liczba kolorow ktore sa wazne: "<<Picture.ClrImportant<<endl;
 
     fclose(f);
     }
